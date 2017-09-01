53cbb1a1d6a7f3bae2282ca975872cf6d843a39e
soccerJoshNumberNine <blue@smittycraft.com>
2017-08-31 18:12:50 -0700

    Update solver.js

--- script/solver.js
+++ script/solver.js
@@ -23,6 +23,34 @@ function validateCube () {
   return centers.yellow.css('backgroundColor') === 'rgb(255, 255, 0)' && centers.orange.css('backgroundColor') === 'rgb(255, 159, 0)' && centers.blue.css('backgroundColor') === 'rgb(20, 38, 173)' && centers.red.css('backgroundColor') === 'rgb(173, 20, 20)' && centers.green.css('backgroundColor') === 'rgb(0, 145, 0)' && centers.white.css('backgroundColor') === 'rgb(255, 255, 255)';
 }
 
+// An object storing more objects in which pieces' posistions and more are held
+// If you don't know how to solve a Rubik's Cube, this cube will make NO sense. I would recommend you understand the Rubik's cube before messing with the code.
+var positions = {
+  corners: {
+    wrb: null,
+    wbo: null,
+    wog: null,
+    wgr: null,
+    ybr: null,
+    yrg: null,
+    ygo: null,
+    yob: null
+  },
+  
+  edges: {
+    wr: null,
+    wb: null,
+    wo: null,
+    wg: null,
+    yb: null,
+    yr: null,
+    yg: null,
+    yo: null
+  }
+};
+
+
+
 $('#solve').click(function () {
   if (validateCube()) {
     
