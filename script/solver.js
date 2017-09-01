aa375eca7323126180dd7d2c8b1a8c4d2f4bf44b
soccerJoshNumberNine <blue@smittycraft.com>
2017-08-31 17:00:17 -0700

    Update solver.js

--- script/solver.js
+++ script/solver.js
@@ -21,9 +21,13 @@ function validateCube () {
     white: $('#49')
   };
   
-  return centers.yellow.css('backgroundColor') === 'rgb(255, 255, 0)' && centers.orange.css('backgroundColor') === 'rgb(255, 159, 0)' && centers.blue.css('backgroundColor') === 'rgb(20, 38, 173)' && centers.red.css('backgroundColor') === 'rgb(173, 20, 20)' && centers.green.css('backgroundColor') === 'rgb(0, 145, 0)';
+  return centers.yellow.css('backgroundColor') === 'rgb(255, 255, 0)' && centers.orange.css('backgroundColor') === 'rgb(255, 159, 0)' && centers.blue.css('backgroundColor') === 'rgb(20, 38, 173)' && centers.red.css('backgroundColor') === 'rgb(173, 20, 20)' && centers.green.css('backgroundColor') === 'rgb(0, 145, 0)' && centers.white.css('backgroundColor') === 'rgb(255, 255, 255)';
 }
 
 $('#solve').click(function () {
-  
+  if (validateCube()) {
+    alert('Cube is valid.');
+  } else {
+    alert('Uh oh, your cube is invalid. Make sure you didn\'t change the color of any center pieces, and that you have nine of each color on the cube.');
+  }
 });
